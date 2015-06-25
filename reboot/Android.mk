6ce8fd6657f80af2e42f03e95786edb9e8a6c5a1
Harvey Chapman <hchapman@realdigitalmedia.com>
2015-06-24 22:15:43 -0400

    add /sbin/poweroff which is a static version of reboot

--- reboot/Android.mk
+++ reboot/Android.mk
@@ -10,3 +10,15 @@ LOCAL_SHARED_LIBRARIES:= libcutils
 LOCAL_MODULE:= reboot
 
 include $(BUILD_EXECUTABLE)
+
+# /sbin/poweroff
+include $(CLEAR_VARS)
+LOCAL_MODULE := poweroff
+LOCAL_SRC_FILES := poweroff.c
+LOCAL_STATIC_LIBRARIES := libc libcutils
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_CFLAGS += $(targetSmpFlag)
+LOCAL_C_INCLUDES := $(libcutils_c_includes)
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)/sbin
+LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_UNSTRIPPED)/sbin
+include $(BUILD_EXECUTABLE)
